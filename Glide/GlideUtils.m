22865d534134d5991e95da9a8b2fe32f362ce882
Axel Etcheverry <euskadi31@gmail.com>
2016-02-12 18:45:48 +0100

    refact(GlideUtils): md5 function

--- Glide/GlideUtils.m
+++ Glide/GlideUtils.m
@@ -22,16 +22,16 @@
 
 + (nullable NSString *)md5:(nonnull NSString *)value {
     const char *cstr = [value UTF8String];
-    unsigned char result[16];
-    CC_MD5(cstr, (uint)strlen(cstr), result);
+    unsigned char digest[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(cstr, (CC_LONG)strlen(cstr), digest);
     
-    return [[NSString stringWithFormat:
-             @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
-             result[0], result[1], result[2], result[3],
-             result[4], result[5], result[6], result[7],
-             result[8], result[9], result[10], result[11],
-             result[12], result[13], result[14], result[15]
-             ] lowercaseString];
+    NSMutableString *md5 = [[NSMutableString alloc] initWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    
+    for (NSInteger i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+        [md5 appendFormat: @"%02x", (int)digest[i]];
+    }
+    
+    return [md5 lowercaseString];
 }
 
 @end
